e865acaedc02c827f19146d636245fb356748350
Abir Roy <abirkumarroy88@gmail.com>
2016-04-07 19:21:44 +0600

    AboutUs

---
+++ contact_us.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/html">
+<head>
+    <link href="resources/css/bootstrap.css" rel='stylesheet' type='text/css' />
+    <script src="resources/js/jquery.min.js"></script>
+    <script src="resources/js/new_drugbd.js"></script>
+    <link href="resources/css/new_drugdb.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+    <link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Arimo" />
+</head>
+<body>
+
+
+<section class="container-fluid">
+    <div class="row">
+        <div class="col-xs-12 col-lg-10 col-sm-12 col-lg-offset-1"  style="margin-top: 50px;">
+            <p>Home / Contact Us</p><br>
+            <img class="image-responsive"src="resources/images/Banner.png" alt="map preview" width="100%">
+
+
+            <table class="table" id="contactUsHead">
+                <tbody>
+                    <tr>
+                        <th>CONTACT US</th>
+                        <th class="text-center">ADDRESS</th>
+                    </tr>
+
+                </tbody>
+            </table>
+
+
+
+
+
+
+        <div class="col-xs-3">
+
+            <div class="form-group">
+              <input type="text" class="form-control margined" id="usr" placeholder="Name*">
+              <input type="text" class="form-control margined" id="usr" placeholder="Email*">
+              <input type="text" class="form-control margined" id="usr" placeholder="Subject*">
+            </div>
+        </div>
+
+        <div class="col-xs-5">
+            <div class="form-group">
+                <textarea class="form-control" rows="5" id="message" placeholder="Message"></textarea>
+            </div><button type="button" class="btn btn-drugBd pull-right">Send Message</button>
+        </div>
+
+
+        <div class="col-xs-4">
+
+            <p>Address</p><p class="text-muted"> 123 Uttara Model Town, Sector 11</p>
+            <br>
+            <p>Phone Number</p><p class="text-muted">880 123 4567</p>
+            <br>
+            <p>Shop Hours</p><br><p class="text-muted">Monday - Friday 10AM - 8PM</p>
+        </div>
+
+    </div>
+</section>
+
+
+
+
+
+<script>document.body.style.backgroundColor = "white";</script>
+</body>
+</html>
